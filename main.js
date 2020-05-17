aea66f1d151d2b0a1b92476255c5b5273bbac5a4
博奕 黄 <1853308@tongji.edu.cn>
2020-05-17 20:45:21 +0800

    Update main.js
    用制表符代替空格

--- main.js
+++ main.js
@@ -3,57 +3,59 @@
 	group name, local cache path, source video path, source webpage path
 */
 const videoSrcData = `
-        舞龙
-            2019
-                全国龙狮锦标赛男子规定, ./video/2019/全国舞龙舞狮锦标赛/规定/20191110wlgd.mp4, http://videoflv.sports.cn/201911/20191110wlgd.mp4, http://dragonlion.sport.org.cn/home/zhxw/2019-11-21/542357.html
-                    南京理工大学, 45, 570
-                        开场造型, 68, 124
-                        绕场跳龙五次, 126, 152
-                        正反腾跃前进, 156, 176
-                        快速游龙, 180, 190
-                        连续快腾进三次, 190, 210
-                        造型（尾盘，龙头戏珠）, 210, 230
-                        大横8字花（成型四次）, 230, 260
-                        左右螺旋跳龙接摇船, 260, 281
-                        大立圆螺旋行进, 282, 305
-                        起伏圆盘, 305, 321
-                        双杆, 321, 357
-                        造型（菱形）, 356, 363
-                        换位跳龙, 363, 377
-                        绕身舞龙, 377, 386
-                        站腿, 386, 397
-                        起伏圆盘接穿龙尾, 397, 406
-                        卧龙腾飞, 405, 414
-                        躺肩挂腰（直躺挂腰）, 413, 427
-                        造型（立龙）, 427, 448
-                        龙脱衣, 445, 455
-                        跳龙行进, 458, 476
-                        斜盘跳龙连续三次, 475, 490
-                        原地螺旋跳龙连续五次接直躺, 490, 509
-                        造型（龙腾九霄）, 509, 533
-                    湖南师范大学, 592, 1533
-                    湖北队, 1597, 2133
-                    台州学院, 2195, 2723
-                    上海三林, 2791, 3300
-                    集美大学, 3348, 3888
-                全国龙狮锦标赛女子传统, ./video/2019/全国舞龙舞狮锦标赛/传统/20191109ctwl.mp4, http://videoflv.sports.cn/201911/20191109ctwl.mp4, http://dragonlion.sport.org.cn/home/zhxw/2019-11-21/542338.html
-                    常州大学, 24, 542
-                    扬州工职院, 644, 1151
-                    江阴职院, 1335, 1841
-                    湖北队, 1929, 2463
-                全国龙狮锦标赛男子传统, ./video/2019/全国舞龙舞狮锦标赛/传统/20191109ctwl.mp4, http://videoflv.sports.cn/201911/20191109ctwl.mp4, http://dragonlion.sport.org.cn/home/zhxw/2019-11-21/542338.html
-                    南理工紫金学院, 2499, 2870
-                    扬州工职院, 3026, 3512
-                    常州大学, 6026, 6555
-            2018
-                亚洲龙狮锦标赛
-                    台湾, ./video/2018/亚洲舞龙舞狮锦标赛/台湾队.mp4, https://vdept.bdstatic.com/444d344556374a725846455836434e45/4776724a7577736b/f4c86b25279a6eaa775b4d3a042aa451a2d6cc923c0450266f1f009b98283a128e8840db7382378714a5db1d4eeae572.mp4?auth_key=1589559633-0-0-ced45b37f19a492a2ee0c77633a4f81b, https://haokan.baidu.com/v?vid=1349428518790328525&pd=bjh&fr=bjhauthor&type=video, 0, 545
-            2017
-                全国运动会
-                    华东交通大学, ./video/2017/全国运动会群众比赛项目/华东交通大学.mp4, https://apd-67bf74619d6c7a48ac8124737de6c033.v.smtcdns.com/om.tc.qq.com/A1hb4tnPq1wr7VwaejiBKxaxfPq2TJeTVWew0mVBQxWs/uwMROfz2r5zBIaQXGdGnC2dfJ7xNfFa-OUQWNxqkLPBi52XA/h0394te13o9.p712.1.mp4?sdtfrom=v1010&amp;guid=a69a6db3fe1251fb13e1b0d4970313b2&amp;vkey=B1D2EC46A6FD127263D4A9E6EA7B36E14FE1FDD5892DAA92AA91AC41FF104BF976998379C3E95B4760528F98A98ABF6AA69BB48ED463D53ACC7D00A64EC8C0E8EF2DC3758C9F767640DA70BF67B04AFCFCAA3F93886C81227028A1B3D10EEDA408BF9A47898CD3E83ADC6F7A62AC393E82AC17F4FF33A110529C4C9CC9F87D17, https://v.qq.com/x/page/h0394te13o9.html
-        舞狮
-                `;
-const difficultyA = ``;
+舞龙
+	2019
+		全国龙狮锦标赛男子规定, ./video/2019/全国舞龙舞狮锦标赛/规定/20191110wlgd.mp4, http://videoflv.sports.cn/201911/20191110wlgd.mp4, http://dragonlion.sport.org.cn/home/zhxw/2019-11-21/542357.html
+			南京理工大学, 45, 570
+				开场造型, 68, 124
+				绕场跳龙五次, 126, 152
+				正反腾跃前进, 156, 176
+				快速游龙, 180, 190
+				连续快腾进三次, 190, 210
+				造型（尾盘，龙头戏珠）, 210, 230
+				大横8字花（成型四次）, 230, 260
+				左右螺旋跳龙接摇船, 260, 281
+				大立圆螺旋行进, 282, 305
+				起伏圆盘, 305, 321
+				双杆, 321, 357
+				造型（菱形）, 356, 363
+				换位跳龙, 363, 377
+				绕身舞龙, 377, 386
+				站腿, 386, 397
+				起伏圆盘接穿龙尾, 397, 406
+				卧龙腾飞, 405, 414
+				躺肩挂腰（直躺挂腰）, 413, 427
+				造型（立龙）, 427, 448
+				龙脱衣, 445, 455
+				跳龙行进, 458, 476
+				斜盘跳龙连续三次, 475, 490
+				原地螺旋跳龙连续五次接直躺, 490, 509
+				造型（龙腾九霄）, 509, 533
+			湖南师范大学, 592, 1533
+			湖北队, 1597, 2133
+			台州学院, 2195, 2723
+			上海三林, 2791, 3300
+			集美大学, 3348, 3888
+		全国龙狮锦标赛女子传统, ./video/2019/全国舞龙舞狮锦标赛/传统/20191109ctwl.mp4, http://videoflv.sports.cn/201911/20191109ctwl.mp4, http://dragonlion.sport.org.cn/home/zhxw/2019-11-21/542338.html
+			常州大学, 24, 542
+			扬州工职院, 644, 1151
+			江阴职院, 1335, 1841
+			湖北队, 1929, 2463
+		全国龙狮锦标赛男子传统, ./video/2019/全国舞龙舞狮锦标赛/传统/20191109ctwl.mp4, http://videoflv.sports.cn/201911/20191109ctwl.mp4, http://dragonlion.sport.org.cn/home/zhxw/2019-11-21/542338.html
+			南理工紫金学院, 2499, 2870
+			扬州工职院, 3026, 3512
+			常州大学, 6026, 6555
+	2018
+		亚洲龙狮锦标赛
+			台湾, ./video/2018/亚洲舞龙舞狮锦标赛/台湾队.mp4, https://vdept.bdstatic.com/444d344556374a725846455836434e45/4776724a7577736b/f4c86b25279a6eaa775b4d3a042aa451a2d6cc923c0450266f1f009b98283a128e8840db7382378714a5db1d4eeae572.mp4?auth_key=1589559633-0-0-ced45b37f19a492a2ee0c77633a4f81b, https://haokan.baidu.com/v?vid=1349428518790328525&pd=bjh&fr=bjhauthor&type=video, 0, 545
+	2017
+		全国运动会
+			华东交通大学, ./video/2017/全国运动会群众比赛项目/华东交通大学.mp4, https://apd-67bf74619d6c7a48ac8124737de6c033.v.smtcdns.com/om.tc.qq.com/A1hb4tnPq1wr7VwaejiBKxaxfPq2TJeTVWew0mVBQxWs/uwMROfz2r5zBIaQXGdGnC2dfJ7xNfFa-OUQWNxqkLPBi52XA/h0394te13o9.p712.1.mp4?sdtfrom=v1010&amp;guid=a69a6db3fe1251fb13e1b0d4970313b2&amp;vkey=B1D2EC46A6FD127263D4A9E6EA7B36E14FE1FDD5892DAA92AA91AC41FF104BF976998379C3E95B4760528F98A98ABF6AA69BB48ED463D53ACC7D00A64EC8C0E8EF2DC3758C9F767640DA70BF67B04AFCFCAA3F93886C81227028A1B3D10EEDA408BF9A47898CD3E83ADC6F7A62AC393E82AC17F4FF33A110529C4C9CC9F87D17, https://v.qq.com/x/page/h0394te13o9.html
+舞狮
+`;
+const difficultyA = `
+
+`;
 function Node(value) {
 	this.value = value.split(',');
 	this.sub = new Array(0);
