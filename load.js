b86afe2aecea8e744b41ede5d375131f6a9342c6
博奕 黄 <1853308@tongji.edu.cn>
2020-05-18 15:55:30 +0800

    通过下部文字控制亮度模式

--- load.js
+++ load.js
@@ -1,3 +1,39 @@
 if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+    turnOff();
+else if (window.matchMedia("(prefers-color-scheme: light)").matches)
+    turnOn();
+else if (window.matchMedia("(prefers-color-scheme: no-preference)").matches)
+    turnOn();
+else {
+    var date = new Date();
+    if (date.getHours() >= 22 || date.getHours() <= 6)
+        turnOff();
+    else turnOn();
+}
+
+var on;
+
+function turnOn() {
+    linkSchemeStyle.href = "./light.css";
+    on = true;
+}
+
+function turnOff() {
     linkSchemeStyle.href = "./dark.css";
-else linkSchemeStyle.href = "./light.css";
+    on = false;
+}
+
+function asDark() {
+    turnOff();
+    lightIndication.innerText = "一熄灯一切被纪念";
+}
+
+function asLight() {
+    turnOn();
+    lightIndication.innerText = "一开灯一切变妄念";
+}
+
+function change() {
+    if (on) asDark();
+    else asLight();
+}
