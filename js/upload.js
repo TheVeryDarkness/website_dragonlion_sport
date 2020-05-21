7931828b3462a6ed51d46d5059a3fa7ffb330a87
博奕 黄 <1853308@tongji.edu.cn>
2020-05-21 10:51:53 +0800

    臣妾做不到啊

--- js/upload.js
+++ js/upload.js
@@ -11,12 +11,24 @@ function upload() {
         }
     }
     */
-    xmlhttp.open("POST", "https://openapi.baidu.com/oauth/2.0/authorize?\
-    response_type=code&\
-    client_id=M0aXGWKPijslrlbObC2A6mLK&\
-    redirect_uri=oob&\
-    scope=basic,netdisk", false);
-    xmlhttp.setRequestHeader("Access-Control-Allow-Origin", " https://openapi.baidu.com");
+    /*
+     xmlhttp.open("POST", "https://openapi.baidu.com/oauth/2.0/authorize?\
+     response_type=code&\
+     client_id=M0aXGWKPijslrlbObC2A6mLK&\
+     redirect_uri=oob&\
+     scope=basic,netdisk", false);
+     */
+    xmlhttp.open("POST", "https://openapi.baidu.com/rest/2.0/passport/users/getInfo?\
+    access_token=121.0c569b3a3855809613f1e363ce50f6a7.Ysvv7yEy--di2GMIpygTt2QslN4ikhscdHmU0sL._nEJfw", false);
+    //xmlhttp.withCredentials = true;
+    xmlhttp.setRequestHeader('Content-Type', 'application/json')
+    /*
+    xmlhttp.setRequestHeader('Access-Control-Max-Age', '3628800');
+    xmlhttp.setRequestHeader('Access-Control-Allow-Methods', 'GET, PUT');
+    xmlhttp.setRequestHeader('Access-Control-Allow-Headers', 'content-type');
+    xmlhttp.setRequestHeader("Access-Control-Allow-Origin", "https://openapi.baidu.com");
+    xmlhttp.setRequestHeader("origin", "https://openapi.baidu.com");
+    */
     xmlhttp.send();
     alert(xmlhttp.responseText);
 }
