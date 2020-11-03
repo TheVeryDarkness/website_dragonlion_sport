c362bdd88d95823f7ccfbcf438f1f243834b9400
博奕 黄 <1853308@tongji.edu.cn>
2020-11-03 21:40:14 +0800

    Update getVideoURL.js
    More approached

--- js/getVideoURL.js
+++ js/getVideoURL.js
@@ -16,7 +16,11 @@
 		sub
 */
 function fetchVideoSrc() {
-	return fetch('https://raw.githubusercontent.com/TheVeryDarkness/sport_data/main/video.json', {
-		method: "GET"
-	}).then(res => { return res.json(); })
+	return fetch('data/video.json', { method: "GET" })
+		.then(res => { return res.json(); })
+		.catch(e => {
+			console.error(e);
+			return fetch('https://raw.githubusercontent.com/TheVeryDarkness/sport_data/main/video.json', { method: "GET" })
+				.then(res => { return res.json(); });
+		});
 }
