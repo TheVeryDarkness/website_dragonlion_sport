75284888436da3d455684ac95efdc19a816992af
博奕 黄 <1853308@tongji.edu.cn>
2020-11-15 09:30:07 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,10 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.css$/, loader: "css-loader"
+				test: /\.css$/,
+				use: [
+					"style-loader", "css-loader?minimize"
+				]
 			}
 		]
 	}
